277f32a674a3ad571b1d8216df2d10018c7260c3
Juhaščik Jonatan <juhascikjonatan@MacBook-Pro-uzivatela-Juhascik.local>
2019-02-02 06:05:55 +0100

    jonkojefrajer

---
+++ cezarovasifra/cezarovasifra/cezerova_sifra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cezarovasifra
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = "jonkojefrajerlebomaradmorskekonky";
+            Console.WriteLine("Povodna sprava; {0}", s);
+            string sprava = "";
+            int posun = 1;
+
+            foreach (char c in s)
+            {
+                int i = (int)c;
+                i += posun;
+                char znak = (char)i;
+                sprava += znak;
+
+            }
+            Console.WriteLine("Zasifrovana sprava: {0}", sprava);
+            Console.ReadKey();
+
+        }
+    }
+}
